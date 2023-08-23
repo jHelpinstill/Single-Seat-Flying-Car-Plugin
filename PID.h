b9a07e38c0273f0fd6a06b1dbf1be3bbbc4d3270
jHelpinstill <jakob.helpinstill@gmail.com>
2023-08-23 17:12:55 -0500

    Add option for reduced P when PID error is near zero

--- PID.h
+++ PID.h
@@ -17,10 +17,15 @@ public:
 	float accumulator_lower_bound{};
 	float accumulator_upper_bound{};
 
+	float reduced_p{};
+	float reduction_radius{};
+
 	PID() {}
 	PID(float p, float d, float i);
 	PID(float p, float d, float i, float accumulator_radius);
 	PID(float p, float d, float i, float lower_bound, float upper_bound);
+	PID(float p, float d, float i, float accumulator_radius, float reduced_p, float reduction_radius);
+
 	float update(float setPoint, float state, float dt); // setPoint: desired output, state: actual output
 	void reset();
 };
